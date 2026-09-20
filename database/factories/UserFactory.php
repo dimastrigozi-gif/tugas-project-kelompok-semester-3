b5f8c40d9cab0f904bc06883e94aef80bab0ee15
hendrayudha0903 <hendrayudha0903@gmail.com>
2026-09-20 19:31:53 +0700

    Fix Pint style issues

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -46,8 +46,8 @@ class UserFactory extends Factory
     /**
      * Indicate that the model has two-factor authentication configured.
      */
-   public function withTwoFactor(): static
-{
-    return $this;
-}
+    public function withTwoFactor(): static
+    {
+        return $this;
+    }
 }
