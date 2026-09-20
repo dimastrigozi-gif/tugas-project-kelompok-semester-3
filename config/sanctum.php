ffb2474a68ea350e9d6a9968eff7e655a565947c
hendrayudha0903 <hendrayudha0903@gmail.com>
2026-09-20 19:03:30 +0700

    Fix Sanctum PHPStan error

--- config/sanctum.php
+++ config/sanctum.php
@@ -18,12 +18,13 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort(),
-        // Sanctum::currentRequestHost(),
-    ))),
+    
+'stateful' => explode(',', (string) env('SANCTUM_STATEFUL_DOMAINS', sprintf(
+    '%s%s',
+    'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+    Sanctum::currentApplicationUrlWithPort(),
+    // Sanctum::currentRequestHost(),
+))),
 
     /*
     |--------------------------------------------------------------------------
